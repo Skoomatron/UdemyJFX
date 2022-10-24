ffe38144aa2cb12b097307c0d70837f9c86844de
Skoomatron <trevor.jeffrey.edwards@gmail.com>
2022-10-24 14:20:31 -0400

    Disabled XML - Manually Appending Elements

--- src/main/java/com/example/udemyjfx/HelloApplication.java
+++ src/main/java/com/example/udemyjfx/HelloApplication.java
@@ -1,8 +1,10 @@
 package com.example.udemyjfx;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,8 +12,16 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        GridPane root = new GridPane();
+        root.setAlignment(Pos.CENTER);
+        root.setHgap(10);
+        root.setVgap(10);
+
+        Label greeting = new Label("Welcome to the Thunderdome!");
+        root.getChildren().add(greeting);
+
+        Scene scene = new Scene(root, 600, 600);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
