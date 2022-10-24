03aa4e696b7d853d43d644823fb98730f3012b63
Skoomatron <trevor.jeffrey.edwards@gmail.com>
2022-10-24 14:31:30 -0400

    Adjusting Fonts with XML

--- src/main/java/com/example/udemyjfx/HelloApplication.java
+++ src/main/java/com/example/udemyjfx/HelloApplication.java
@@ -1,13 +1,8 @@
 package com.example.udemyjfx;
 
 import javafx.application.Application;
-import javafx.geometry.Pos;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,18 +10,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        GridPane root = new GridPane();
-        root.setAlignment(Pos.CENTER);
-        root.setHgap(10);
-        root.setVgap(10);
-
-        Label greeting = new Label("Welcome to the Thunderdome!");
-        greeting.setTextFill(Color.GREEN);
-        greeting.setFont(Font.font("Times New Roman", FontWeight.BOLD, 50));
-        root.getChildren().add(greeting);
-
-        Scene scene = new Scene(root, 1200, 1200);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 600);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
