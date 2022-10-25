ce288dcd684f8c7bea7ec37784353b3ff54d4c5d
Skoomatron <trevor.jeffrey.edwards@gmail.com>
2022-10-25 11:46:38 -0400

    Text and Button Events Handled

--- src/main/java/com/example/udemyjfx/HelloController.java
+++ src/main/java/com/example/udemyjfx/HelloController.java
@@ -1,7 +1,12 @@
 package com.example.udemyjfx;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class HelloController {
+    @FXML
+    private TextField mainField;
     public void onButtonClick() {
-        System.out.println("Yeeeeeehaw!!!");
+        System.out.println("Yeeeeee " + mainField.getText());
     }
 }
