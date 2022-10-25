a36a33950f6adbf70b450a97c4bac25828f0a21f
Skoomatron <trevor.jeffrey.edwards@gmail.com>
2022-10-25 11:56:10 -0400

    Conditional Checked To Button Click Event

--- src/main/java/com/example/udemyjfx/HelloController.java
+++ src/main/java/com/example/udemyjfx/HelloController.java
@@ -1,12 +1,24 @@
 package com.example.udemyjfx;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
     private TextField mainField;
-    public void onButtonClick() {
-        System.out.println("Yeeeeee " + mainField.getText());
+    @FXML
+    private Button button1;
+    @FXML
+    private Button button2;
+
+    @FXML
+    public void onButtonClick(ActionEvent e) {
+        if (e.getSource().equals(button1)) {
+            System.out.println("Button1: " + mainField.getText());
+        } else if (e.getSource().equals(button2)) {
+            System.out.println("Button2: " + mainField.getText());
+        }
     }
 }
